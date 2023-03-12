3c02440b8e26e864a5a36bb6f4b6262b8393013e
TianyuanZhang001 <tianyuan.zhang@my.com>
2023-03-12 15:49:43 +0000

    interactive-VFX-data controlled

---
+++ Assets/interactive shadergraph+VFX/c-point/VFXcontroller.cs
@@ -0,0 +1,33 @@
+
+using UnityEngine;
+using UnityEngine.VFX;
+
+
+public class VFXcontroller : MonoBehaviour
+{
+    public VisualEffect vfxGraph; // Reference to the VFX graph
+
+    // Name of the float data to control
+   
+
+    public float newfloatvalue;
+
+    void Update()
+    {
+        // Get the value of the float data
+        float xcale = vfxGraph.GetFloat("Rate");
+
+        //print
+        Debug.Log("Rate is" + xcale);
+
+
+        if (Input.GetKeyDown(KeyCode.Alpha9))
+        {
+
+            newfloatvalue += Time.deltaTime;
+
+            vfxGraph.SetFloat("Rate", newfloatvalue);
+        }
+
+    }
+}
