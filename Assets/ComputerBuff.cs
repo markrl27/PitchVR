5dfbc5b2fc76adc09eb59eb2e3b5c5f803cd5a9b
TianyuanZhang001 <tianyuan.zhang@my.com>
2023-03-27 18:14:28 +0100

    textVFX+fixVFX

---
+++ Assets/ComputerBuff.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComputerBuff : MonoBehaviour
+{
+
+    public ComputeShader computeShader;
+    public int bufferSize;
+
+    void Start()
+    {
+        // Create a new ComputeBuffer with the desired size
+        ComputeBuffer buffer = new ComputeBuffer(bufferSize, sizeof(float));
+
+        // Set the buffer at index 4 in the compute shader
+        computeShader.SetBuffer(0, 4, buffer);
+    }
+}
